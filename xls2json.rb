819f05f59011898ea6b747900f8c3cb0c03db37b
yuanhang <yuanhang.guo@gmail.com>
2013-06-15 23:54:20 +0800

    Add class event

--- xls2json.rb
+++ xls2json.rb
@@ -1,31 +1,43 @@
 require 'spreadsheet'
 
 class Event
+  #class << self
+    #attr_accessor :attributes
+  #end
+  @@attributes = [:id, :content, :strategy, :product, :tech, :operation, :fortune, :range, :reusable, :comment]
+  @@attributes.each { |attr| attr_accessor attr }
+  def self.attributes
+    @@attributes
+  end
+  def key attr
+    instance_variable_get "@#{attr}"
+  end
+  #attr_accessor :id, :content, :strategy, :product, :tech, :operation, :fortune, :range, :reusable, :comment
   def initialize values  
     i = 0
-    @id = Integer(values[i]);
-    ++i
+    @id = values[i].to_i;
+    i += 1
     @content = values[i]
-    ++i
-    @strategy = convert_to_point values[i]
-    ++i
-    @product = convert_to_point values[i]
-    ++i
-    @tech = convert_to_point values[i]
-    ++i
-    @operation = convert_to_point values[i]
-    ++i
-    @fortune = convert_to_point values[i]
-    ++i
+    i += 1
+    @strategy = values[i].to_i
+    i += 1
+    @product = values[i].to_i
+    i += 1
+    @tech = values[i].to_i
+    i += 1
+    @operation = values[i].to_i
+    i += 1
+    @fortune = values[i].to_i
+    i += 1
     @range = values[i]
-    ++i
+    i += 1
     @reusable = values[i]
-    ++i
+    i += 1
     @comment = values[i]
   end
 
   def convert_to_point str
-    if String.empty?
+    if str.empty?
       return 0
     else
       return str.to_i
@@ -46,8 +58,9 @@ puts keys
 rows = Array.new
 events = Array.new
 sheet1.each omit_rows do |row|
-  rows << row
-  events << Event.new row
+  rows << row;
+  events << Event.new(row)
+  #puts events.last.to_s
 end
 
 # Create output json file
@@ -57,16 +70,24 @@ File.delete(json_file_name) if File.exist?(json_file_name)
 json_file = File.open(json_file_name, 'w')
 json_file << "[\n"
 
-rows.each_with_index do |row, index|
+#rows.each_with_index do |row, index1|
+#json_file << "{\n"
+#Event.attributes.each_with_index do |key, index|
+#json_file << "#{key}:#{row[index]},\n"
+##puts row[index]
+#end
+#json_file << "},\n"
+#end
+events.each_with_index do |event, index1|
   json_file << "{\n"
-  keys.each_with_index do |key, index|
-    json_file << "#{key}:#{row[index]},\n"
-    puts row[index]
+  Event.attributes.each_with_index do |key, index|
+    json_file << "#{key}:#{event.key(key)},\n"
+    #puts row[index]
   end
   json_file << "},\n"
 end
 
 json_file << "]\n"
 json_file.close
-puts events
+puts events.to_s
 
