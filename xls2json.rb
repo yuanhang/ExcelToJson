a2870c8f2f53433c8df8f417877ad70c7c319f65
yuanhang <yuanhang.guo@gmail.com>
2013-06-16 12:11:57 +0800

    add eventBags in json

--- xls2json.rb
+++ xls2json.rb
@@ -1,24 +1,75 @@
 require 'spreadsheet'
+require 'singleton'
+
+class EventBagManager
+  include Singleton
+
+  attr_accessor :bags
+
+  def initialize
+    @bags = Hash.new
+  end
+
+  def add_event event
+    if @bags.has_key? event.id
+      @bags[event.id].add_event(event)
+    else
+      @bags[event.id] = EventBag.new(event)
+    end
+  end
+
+  def to_json
+    json_str = "{\"eventBags\":["
+    @bags.to_a.each_with_index do |pair, i|
+      json_str += pair[1].to_json
+      json_str += "," unless i == @bags.size-1
+    end
+    json_str += "]}"
+  end
+end
+
+class EventBag
+  attr_accessor :events, :id
+  def initialize event
+    @events = Array.new
+    add_event(event)
+  end
+
+  def add_event event
+    if @events.empty?
+      @id = event.id
+      @events << event
+      puts "new bag: #{event.id}"
+    else
+      if event.id == @id
+        @events << event
+        puts "add to bag: #{event.id}"
+      end
+    end
+  end
+
+  def to_json
+    json_str = "{\"id\":\"#{@id}\","
+    json_str += "\"events\":["
+    events.each_with_index do |event, i|
+      json_str += "{#{event.to_json}}" 
+      json_str += "," unless i == events.size-1
+    end
+    json_str += "]}"
+  end
+end
 
 class Event
-  #class << self
-  #attr_accessor :attributes
-  #end
   @@attributes = [:id, :title, :content, :strategy, :product, :tech, :operation, :fortune, :range, :reusable, :comment]
   @@attributes.each { |attr| attr_accessor attr }
-  def self.attributes
-    @@attributes
-  end
   def value_for_key key
     instance_variable_get "@#{key}"
   end
+
   def initialize values  
     i = 0
     @id = values[i].to_i;
     i += 1
-    if (values[i] == nil)
-      puts @id
-    end
     @title = values[i].split("\n")[0]
     @content = values[i].split("\n")[1]
     i += 1
@@ -59,8 +110,9 @@ omit_rows = 2
 keys_row_index = 1
 
 events = Array.new
+
 sheet1.each omit_rows do |row|
-  events << Event.new(row)
+  EventBagManager.instance.add_event(Event.new(row))
 end
 
 # Create output json file
@@ -68,13 +120,16 @@ json_file_name = xls_file_name.split('.')[0] + '.json'
 File.delete(json_file_name) if File.exist?(json_file_name)
 
 json_file = File.open(json_file_name, 'w')
-json_file << "{\"events\":["
-events.each_with_index do |event, i|
-  json_str = "{#{event.to_json}}" 
-  json_str += "," unless i == events.size-1
-  json_file << json_str
-end
-json_file << "]}"
+json_file << EventBagManager.instance.to_json
+#json_file << "{\"eventBags\":["
+#EventBagManager.instance.bags.each do |id, bag|
+#bag.events.each_with_index do |event, i|
+#json_str = "{#{event.to_json}}" 
+#json_str += "," unless i == events.size-1
+#json_file << json_str
+#end
+#break
+#end
+#json_file << "]}"
 json_file.close
-puts events[0].to_json
 
